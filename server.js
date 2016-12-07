a8077a9e933e44bfbd1b89ab1b8cc48a310efbbc
Danny Pernik <dannypernik@gmail.com>
2016-12-07 04:55:09 -0600

    home

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 7777));
+
+app.use(express.static(__dirname + '/public'));
+app.use('/modules', express.static(__dirname + '/node_modules/'));
+
+app.get('/', function(req, res) {
+    res.render('pages/home');
+});
+
+app.get('/:id', function(req, res) {
+  res.render('pages/' + req.params.id);
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Node app is running on port', app.get('port'));
+});
